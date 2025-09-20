51c3c955df74016ca45ac9d30cfc031041632ca5
Kyle Robinson <ky.robinson72@gmail.com>
2025-09-19 19:33:21 -0600

    (tests passed) Set up the resetBoard method in the ChessBoard.java file and added the Equals and HashCode functions

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Arrays;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -9,9 +11,7 @@ package chess;
 public class ChessBoard {
 
     private final ChessPiece [][] board = new ChessPiece[8][8];
-    public ChessBoard() {
-
-    }
+    public ChessBoard() {}
 
     /**
      * Adds a chess piece to the chessboard
@@ -39,6 +39,35 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        ChessPiece.PieceType[] backRow = {ChessPiece.PieceType.ROOK, ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.BISHOP,
+                        ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.KING, ChessPiece.PieceType.BISHOP,
+                        ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.ROOK};
+
+        for (ChessPiece[] chessPieces : board) {
+            Arrays.fill(chessPieces, null);
+        }
+
+        for (int i = 0; i < backRow.length; i++) {
+            board[0][i] = new ChessPiece(ChessGame.TeamColor.WHITE, backRow[i]);
+            board[7][i] = new ChessPiece(ChessGame.TeamColor.BLACK, backRow[i]);
+        }
+
+        Arrays.fill(board[1], new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+        Arrays.fill(board[6], new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ChessBoard that = (ChessBoard) o;
+        return Arrays.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
     }
 }
