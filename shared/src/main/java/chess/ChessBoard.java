73bb589bcc95c7833ec7498709abdc368e3c28cc
Kyle Robinson <ky.robinson72@gmail.com>
2025-10-03 13:48:14 -0600

    (ChessPieceTests passing) Fixed addPiece function which was having an issue when the piece entered was null. Added checker to just return if piece is null

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -26,6 +26,10 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
+        if (piece == null) {
+            return;
+        }
+
         board[position.getRow() - 1][position.getColumn() - 1] = piece;
 
         if (piece.getPieceType() == ChessPiece.PieceType.KING) {
