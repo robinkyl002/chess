f3f105a8c17d2ced5472b02cc57b2a53ed5b1421
Kyle Robinson <ky.robinson72@gmail.com>
2025-10-28 22:47:53 -0600

    Added a HashMap to hold the Auth data and created functions to add a new auth, retrieve auth, delete one auth or delete all auth data.

--- server/src/main/java/dataaccess/MemoryAuthDataAccess.java
+++ server/src/main/java/dataaccess/MemoryAuthDataAccess.java
@@ -2,25 +2,28 @@ package dataaccess;
 
 import model.AuthData;
 
+import java.util.HashMap;
+
 public class MemoryAuthDataAccess implements AuthDAO {
+    HashMap<String, AuthData> authTokens = new HashMap<>();
 
     @Override
     public void createAuth(AuthData data) throws DataAccessException {
-
+        authTokens.put(data.authToken(), data);
     }
 
     @Override
-    public AuthData getAuth(String username) throws DataAccessException {
-        return null;
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return authTokens.get(authToken);
     }
 
     @Override
-    public void deleteAuth(String username) throws DataAccessException {
-
+    public void deleteAuth(String authToken) throws DataAccessException {
+        authTokens.remove(authToken);
     }
 
     @Override
     public void clearAuthData() {
-
+        authTokens.clear();
     }
 }
