eccb3bf3d479da837163698d5ea297f3b1b1064f
Kyle Robinson <ky.robinson72@gmail.com>
2025-11-07 15:08:34 -0700

    Created ResponseException.java to handle any errors with data input by the user or authorization issues

---
+++ shared/src/main/java/exception/ResponseException.java
@@ -0,0 +1,58 @@
+package exception;
+
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseException extends Exception {
+
+    public enum Code {
+        UnauthorizedError,
+        AlreadyTakenError,
+        BadRequestError,
+        ServerError,
+    }
+
+    final private Code code;
+
+    public ResponseException(Code code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public String toJson() {
+        return new Gson().toJson(Map.of("message", getMessage(), "status", toHttpStatusCode()));
+    }
+
+    public static ResponseException fromJson(String json) {
+        var map = new Gson().fromJson(json, HashMap.class);
+        var status = Code.valueOf(map.get("status").toString());
+        String message = map.get("message").toString();
+        return new ResponseException(status, message);
+    }
+
+    public Code code() {
+        return code;
+    }
+
+    public static Code fromHttpStatusCode(int httpStatusCode) {
+        return switch (httpStatusCode) {
+            case 500 -> Code.ServerError;
+            case 400 -> Code.BadRequestError;
+            case 401 -> Code.UnauthorizedError;
+            case 403 -> Code.AlreadyTakenError;
+            default -> throw new IllegalArgumentException("Unknown HTTP status code: " + httpStatusCode);
+        };
+    }
+
+    public int toHttpStatusCode() {
+        return switch (code) {
+            case ServerError -> 500;
+            case BadRequestError -> 400;
+            case UnauthorizedError -> 401;
+            case AlreadyTakenError -> 403;
+        };
+    }
+}
+
