d68ff6826fa16957cd46fb18b9d541c516ef1308
Kyle Robinson <ky.robinson72@gmail.com>
2025-11-11 12:45:32 -0700

    Added try/catch to each of the functions to throw DataAccessExceptions if there is an issue storing, retrieving, or removing data from the data source

--- server/src/main/java/dataaccess/MemoryAuthDataAccess.java
+++ server/src/main/java/dataaccess/MemoryAuthDataAccess.java
@@ -9,27 +9,41 @@ public class MemoryAuthDataAccess implements AuthDAO {
     HashMap<String, AuthData> authTokens = new HashMap<>();
 
     public AuthData createAuth(String username) throws DataAccessException {
-        //String token = generateToken();
-        AuthData newAuth = new AuthData(generateToken(), username);
-        authTokens.put(newAuth.authToken(), newAuth);
-        return newAuth;
-
-
+        try {
+            AuthData newAuth = new AuthData(generateToken(), username);
+            authTokens.put(newAuth.authToken(), newAuth);
+            return newAuth;
+        } catch (Exception e) {
+            throw new DataAccessException("Error: " + e.getMessage(), e);
+        }
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
-        return authTokens.get(authToken);
+        try {
+            return authTokens.get(authToken);
+        } catch (Exception ex) {
+            throw new DataAccessException(ex.getMessage(), ex);
+        }
     }
 
     @Override
     public void deleteAuth(String authToken) throws DataAccessException {
-        authTokens.remove(authToken);
+        try {
+            authTokens.remove(authToken);
+        }
+        catch (Exception ex) {
+            throw new DataAccessException("Error: " + ex.getMessage(), ex);
+        }
     }
 
     @Override
-    public void clearAuthData() {
-        authTokens.clear();
+    public void clearAuthData() throws DataAccessException {
+        try {
+            authTokens.clear();
+        } catch (Exception e) {
+            throw new DataAccessException("Error: " + e.getMessage(), e);
+        }
     }
 
     public static String generateToken() {
