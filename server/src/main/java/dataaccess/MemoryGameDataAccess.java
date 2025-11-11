fab3eb9aaf8c8d50c8d88d3decaa15ba9540d39f
Kyle Robinson <ky.robinson72@gmail.com>
2025-11-11 12:37:22 -0700

    Set up createGame, getGame, and clearGameData methods with the data structures

--- server/src/main/java/dataaccess/MemoryGameDataAccess.java
+++ server/src/main/java/dataaccess/MemoryGameDataAccess.java
@@ -1,18 +1,26 @@
 package dataaccess;
 
+import chess.ChessGame;
 import model.GameData;
+import model.NewGameResult;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class MemoryGameDataAccess implements GameDAO{
-    @Override
-    public void createGame(GameData data) throws DataAccessException {
+    private int nextId = 1;
+    HashMap<Integer, GameData> games = new HashMap<>();
 
+    @Override
+    public NewGameResult createGame(String gameName) throws DataAccessException {
+        GameData game = new GameData(nextId++, null, null, gameName, new ChessGame());
+        games.put(game.gameID(), game);
+        return new NewGameResult(game.gameID());
     }
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
-        return null;
+        return games.get(gameID);
     }
 
     @Override
@@ -22,6 +30,6 @@ public class MemoryGameDataAccess implements GameDAO{
 
     @Override
     public void clearGameData() {
-
+        games.clear();
     }
 }
