93827e228ec3519b6bc30bcebca606844f971833
Kyle Robinson <ky.robinson72@gmail.com>
2025-11-18 12:43:30 -0700

    Added functions to set each of the usernames for the joinGame function.

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -3,4 +3,12 @@ package model;
 import chess.ChessGame;
 
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+
+    public GameData setBlackUsername(GameData gameData, String blackUsername) {
+        return new GameData(gameData.gameID(), gameData.whiteUsername(), blackUsername, gameData.gameName(), gameData.game());
+    }
+
+    public GameData setWhiteUsername(GameData gameData, String whiteUsername) {
+        return new GameData(gameData.gameID(), whiteUsername, gameData.blackUsername(), gameData.gameName(), gameData.game());
+    }
 }
