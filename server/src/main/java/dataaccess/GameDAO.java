10cdad46a9cf52733398288681ff93f0f5c0a17f
Kyle Robinson <ky.robinson72@gmail.com>
2025-12-06 16:07:24 -0700

    Added a GameData parameter to the updateGame function

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -9,6 +9,6 @@ public interface GameDAO {
     NewGameResult createGame(String data) throws DataAccessException;
     GameData getGame(int gameID) throws DataAccessException;
     HashSet<GameData> listGames() throws DataAccessException;
-    void updateGame() throws DataAccessException;
+    void updateGame(GameData updatedGame) throws DataAccessException;
     void clearGameData() throws DataAccessException;
 }
