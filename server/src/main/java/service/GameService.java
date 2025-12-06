07f911505c0772158bfa6ebc5689e46bfb323408
Kyle Robinson <ky.robinson72@gmail.com>
2025-12-06 16:11:16 -0700

    Added the joinGame function which checks to make sure the player color is not already taken and updates the GameData if the username for that game is not null

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,10 +1,11 @@
 package service;
 
+import chess.ChessGame;
 import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
 import dataaccess.GameDAO;
 import exception.ResponseException;
-import model.NewGameResult;
+import model.*;
 
 public class GameService {
     private final GameDAO gameDataAccess;
@@ -20,5 +21,38 @@ public class GameService {
             throw new ResponseException(ResponseException.Code.BadRequestError, "Error: bad request");
         }
         return gameDataAccess.createGame(gameName);
+    }
+
+    public void joinGame(JoinGameRequest joinRequest, String authToken) throws DataAccessException, ResponseException {
+        if (joinRequest.playerColor() == null || joinRequest.gameID() == null) {
+            throw new ResponseException(ResponseException.Code.BadRequestError, "Error: bad request");
+        }
+        GameData game = gameDataAccess.getGame(joinRequest.gameID());
+
+        if ((joinRequest.playerColor() == ChessGame.TeamColor.BLACK && game.blackUsername() != null) ||
+                (joinRequest.playerColor() == ChessGame.TeamColor.WHITE && game.whiteUsername() != null)) {
+            throw new ResponseException(ResponseException.Code.AlreadyTakenError, "Error: already taken");
+        }
+        AuthData auth = authDataAccess.getAuth(authToken);
+
+        if (joinRequest.playerColor() == ChessGame.TeamColor.BLACK) {
+            GameData updatedGame = game.setBlackUsername(game, auth.username());
+            gameDataAccess.updateGame(updatedGame);
+        }
+        else {
+            GameData updatedGame = game.setWhiteUsername(game, auth.username());
+            gameDataAccess.updateGame(updatedGame);
+        }
+
+
+
+    }
+
+    public void clearGameData() throws ResponseException {
+        try {
+            gameDataAccess.clearGameData();
+        } catch (DataAccessException e) {
+            throw new ResponseException(ResponseException.Code.ServerError, e.getMessage());
+        }
     }
 }
