83ff11b5b215d2ab80de447e1d0640c37443b5bc
Kyle Robinson <ky.robinson72@gmail.com>
2025-12-06 16:28:49 -0700

    Finished building tests for successful and failed login

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -47,13 +47,30 @@ class UserServiceTest {
     @Test
     @DisplayName("Successful Login")
     void login() throws ResponseException, DataAccessException {
-        userService.createUser(existingUser.getUsername(), existingUser.getPassword(), existingUser.getEmail());
+        AuthData registerAuth = userService.createUser(existingUser.getUsername(), existingUser.getPassword(), existingUser.getEmail());
+
+        userService.logout(registerAuth.authToken());
+
+        UserData newUser = new UserData(existingUser.getUsername(), existingUser.getPassword(), existingUser.getEmail());
+
+        AuthData loginAuth = userService.login(newUser);
+
+        assertNotNull(loginAuth);
+        assertEquals(loginAuth.username(), newUser.username(), "Username of the AuthData does not match the username used to login");
     }
 
     @Test
     @DisplayName("Wrong Password")
-    void loginWrongPassword() {
+    void loginWrongPassword() throws ResponseException, DataAccessException {
+        AuthData registerAuth = userService.createUser(existingUser.getUsername(), existingUser.getPassword(), existingUser.getEmail());
 
+        userService.logout(registerAuth.authToken());
+
+        UserData newUser = new UserData(existingUser.getUsername(), "WrongPassword", existingUser.getEmail());
+
+        assertThrows(ResponseException.class, () -> {
+            userService.login(newUser);
+        });
     }
 
     @Test
