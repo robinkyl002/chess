3cf9a298de23b4fe5bbd6a4809a84a83579b5c37
Kyle Robinson <ky.robinson72@gmail.com>
2025-12-06 18:08:53 -0700

    Fixed some code quality issues

--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -2,7 +2,7 @@ package exception;
 
 import com.google.gson.Gson;
 
-import java.util.HashMap;
+// import java.util.HashMap;
 import java.util.Map;
 
 public class ResponseException extends Exception {
@@ -25,26 +25,26 @@ public class ResponseException extends Exception {
         return new Gson().toJson(Map.of("message", getMessage()));
     }
 
-    public static ResponseException fromJson(String json) {
-        var map = new Gson().fromJson(json, HashMap.class);
-        var status = Code.valueOf(map.get("status").toString());
-        String message = map.get("message").toString();
-        return new ResponseException(status, message);
-    }
+//    public static ResponseException fromJson(String json) {
+//        var map = new Gson().fromJson(json, HashMap.class);
+//        var status = Code.valueOf(map.get("status").toString());
+//        String message = map.get("message").toString();
+//        return new ResponseException(status, message);
+//    }
 
-    public Code code() {
-        return code;
-    }
-
-    public static Code fromHttpStatusCode(int httpStatusCode) {
-        return switch (httpStatusCode) {
-            case 500 -> Code.ServerError;
-            case 400 -> Code.BadRequestError;
-            case 401 -> Code.UnauthorizedError;
-            case 403 -> Code.AlreadyTakenError;
-            default -> throw new IllegalArgumentException("Unknown HTTP status code: " + httpStatusCode);
-        };
-    }
+//    public Code code() {
+//        return code;
+//    }
+//
+//    public static Code fromHttpStatusCode(int httpStatusCode) {
+//        return switch (httpStatusCode) {
+//            case 500 -> Code.ServerError;
+//            case 400 -> Code.BadRequestError;
+//            case 401 -> Code.UnauthorizedError;
+//            case 403 -> Code.AlreadyTakenError;
+//            default -> throw new IllegalArgumentException("Unknown HTTP status code: " + httpStatusCode);
+//        };
+//    }
 
     public int toHttpStatusCode() {
         return switch (code) {
