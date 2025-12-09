e313b5792f3df2574d5ce2dfc1fc3f9631a60f00
Kyle Robinson <ky.robinson72@gmail.com>
2025-12-08 20:39:40 -0700

    Added executeUpdate function to handle making changes to the data in the database using prepared statements

--- server/src/main/java/dataaccess/SQLInitializer.java
+++ server/src/main/java/dataaccess/SQLInitializer.java
@@ -3,7 +3,12 @@ package dataaccess;
 import exception.ResponseException;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class SQLInitializer {
 
@@ -53,6 +58,28 @@ public class SQLInitializer {
             }
         } catch (SQLException ex) {
             throw new ResponseException(ResponseException.Code.ServerError, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+
+    public static int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (int i = 0; i < params.length; i++) {
+                    Object param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("unable to update database", e);
         }
     }
 
