943476fe47c6443ad4b3ad0ae415f7a435c334d6
Kyle Robinson <ky.robinson72@gmail.com>
2025-12-08 20:18:02 -0700

    Set up each of the functions for the SQL User data access class

--- server/src/main/java/dataaccess/SQLUserDataAccess.java
+++ server/src/main/java/dataaccess/SQLUserDataAccess.java
@@ -1,33 +1,55 @@
 package dataaccess;
 
+import com.google.gson.Gson;
+import exception.ResponseException;
 import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class SQLUserDataAccess implements UserDAO{
     @Override
     public void createUser(UserData data) throws DataAccessException {
-
+        var statement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+        String hashedPassword = BCrypt.hashpw(data.password(), BCrypt.gensalt());
+        SQLInitializer.executeUpdate(statement, data.username(), hashedPassword, data.email());
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, password, email FROM user WHERE username=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, username);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readUser(rs);
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            throw new DataAccessException("Could not find user in database", ex);
+        }
         return null;
     }
 
     @Override
     public void clearUserData() throws DataAccessException {
-
+        var statement = "TRUNCATE user";
+        SQLInitializer.executeUpdate(statement);
     }
 
-    private final String[] createStatements = {
-            """
-            CREATE TABLE IF NOT EXISTS  user (
-              `username` varchar(256) NOT NULL,
-              `password` varchar(256) NOT NULL,
-              `email` varchar(256) NOT NULL,
-              PRIMARY KEY (`username`),
-              INDEX(email),
-              INDEX(password)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-            """
-    };
+    private UserData readUser(ResultSet rs) throws SQLException {
+        var username = rs.getString("username");
+        var hashedPassword = rs.getString("password");
+        var email = rs.getString("email");
+
+        return new UserData(username, hashedPassword, email);
+    }
 }
