cc3a0425b1521b4f53fbe5e0a5f6dc4e41038e45
Kyle Robinson <ky.robinson72@gmail.com>
2026-01-07 19:21:02 -0700

    addPiece and getPiece are passing the ChessBoard tests

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -25,7 +25,11 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board[position.getRow()-1][position.getColumn()-1] = piece;
+
+        if (piece == null) {
+            return;
+        }
     }
 
     /**
@@ -36,7 +40,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board[position.getRow()-1][position.getColumn()-1];
     }
 
     /**
