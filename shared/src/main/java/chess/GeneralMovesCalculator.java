d94e66ba258b6a7f6efc455377f75aa23e332f70
Kyle Robinson <ky.robinson72@gmail.com>
2026-01-07 20:30:23 -0700

    Implemented a calculateMoves function that will calculate all moves for a certain direction of movement

--- shared/src/main/java/chess/GeneralMovesCalculator.java
+++ shared/src/main/java/chess/GeneralMovesCalculator.java
@@ -1,4 +1,36 @@
 package chess;
 
+import java.util.Collection;
+
 public abstract class GeneralMovesCalculator implements MoveRules{
+    protected void calculateMoves(ChessPosition pos, int rowIncrement, int colIncrement,
+                                  Collection<ChessMove> moves, ChessBoard board,
+                                  boolean repeatMove) {
+        if (pos.getRow() + rowIncrement > 8 || pos.getRow() + rowIncrement < 0 ||
+                pos.getColumn() + colIncrement > 8 || pos.getColumn() + colIncrement < 0) {
+            return;
+        }
+
+        ChessPosition newPosition = new ChessPosition(pos.getRow() + rowIncrement,
+                pos.getColumn() + colIncrement);
+        if (board.getPiece(newPosition) == null) {
+            moves.add(new ChessMove(pos, newPosition, null));
+            if (repeatMove) {
+                calculateMoves(newPosition, rowIncrement + Integer.signum(rowIncrement),
+                        colIncrement + Integer.signum(colIncrement), moves, board, repeatMove);
+            }
+        }
+        else {
+            if (board.getPiece(newPosition).getTeamColor() == board.getPiece(pos).getTeamColor()) {
+                return;
+            }
+            else {
+                moves.add(new ChessMove(pos, newPosition,null));
+                return;
+            }
+        }
+    }
+
+
+    public abstract Collection<ChessMove> moves(ChessBoard board, ChessPosition pos);
 }
