ff9e23101490a65ae002170ed7075f3cf9f79317
Kyle Robinson <ky.robinson72@gmail.com>
2026-01-07 21:02:22 -0700

    Added calls to the calculateMoves function for the other 3 directions of movement performed by a rook

--- shared/src/main/java/chess/RookMoves.java
+++ shared/src/main/java/chess/RookMoves.java
@@ -8,6 +8,9 @@ public class RookMoves extends GeneralMovesCalculator{
     public Collection<ChessMove> moves(ChessBoard board, ChessPosition pos) {
         var moves = new HashSet<ChessMove>();
         calculateMoves(pos, 1, 0, moves, board, true);
+        calculateMoves(pos, -1, 0, moves, board, true);
+        calculateMoves(pos, 0, 1, moves, board, true);
+        calculateMoves(pos, 0, -1, moves, board, true);
         return moves;
     }
 }
