cbff6888a328cdd78951de956d253832b9307fd9
Kyle Robinson <ky.robinson72@gmail.com>
2026-01-12 15:26:53 -0700

    Added the other calls to calculateMoves in the Bishop class

--- shared/src/main/java/chess/BishopMoves.java
+++ shared/src/main/java/chess/BishopMoves.java
@@ -2,14 +2,15 @@ package chess;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 
 public class BishopMoves extends GeneralMovesCalculator{
     @Override
     public Collection<ChessMove> moves(ChessBoard board, ChessPosition pos) {
         var moves = new HashSet<ChessMove>();
         calculateMoves(pos, 1, 1, moves, board, true);
-
+        calculateMoves(pos, 1, -1, moves, board, true);
+        calculateMoves(pos, -1, 1, moves, board, true);
+        calculateMoves(pos, -1, -1, moves, board, true);
         return moves;
     }
 }
