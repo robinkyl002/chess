4003e006b0f540974e0a97af5ca0057197f4802e
Kyle Robinson <ky.robinson72@gmail.com>
2026-01-12 15:28:51 -0700

    Created a KingMoves class to calculate all moves for a king piece

---
+++ shared/src/main/java/chess/KingMoves.java
@@ -0,0 +1,20 @@
+package chess;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class KingMoves extends GeneralMovesCalculator{
+    @Override
+    public Collection<ChessMove> moves(ChessBoard board, ChessPosition pos) {
+        var moves = new HashSet<ChessMove>();
+        calculateMoves(pos, 1, 0, moves, board, false);
+        calculateMoves(pos, -1, 0, moves, board, false);
+        calculateMoves(pos, 0, 1, moves, board, false);
+        calculateMoves(pos, 0, -1, moves, board, false);
+        calculateMoves(pos, 1, 1, moves, board, false);
+        calculateMoves(pos, 1, -1, moves, board, false);
+        calculateMoves(pos, -1, 1, moves, board, false);
+        calculateMoves(pos, -1, -1, moves, board, false);
+        return moves;
+    }
+}
