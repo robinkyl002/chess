c5d799c3db82c3c49b6b4f2a0d2f8a16c054d69c
Kyle Robinson <ky.robinson72@gmail.com>
2026-01-13 16:36:20 -0700

    Filled out functions to get normal forward moves functioning and passing 12 out of 23 tests in PawnMoveTests

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -3,6 +3,7 @@ package chess;
 import java.util.Collection;
 import java.util.HashSet;
 import static chess.ChessPiece.PieceType.*;
+import static chess.ChessGame.TeamColor.*;
 
 public class PawnMovesCalculator implements MoveRules{
     @Override
@@ -11,8 +12,12 @@ public class PawnMovesCalculator implements MoveRules{
         var currPiece = board.getPiece(pos);
         var currColor = currPiece.getTeamColor();
 
-        forwardMoves(moves, board, pos, currColor);
-        diagonalMoves(moves, board, pos, currColor);
+        int direction = (currColor.equals(WHITE)) ? 1 : -1;
+        int initRow = (currColor.equals(WHITE)) ? 2 : 7;
+        int promoRow = (currColor.equals(WHITE)) ? 8 : 1;
+
+        forwardMoves(moves, board, pos, initRow, promoRow, direction);
+        diagonalMoves(moves, board, pos, initRow, promoRow, direction);
 
         return moves;
     }
@@ -31,14 +36,36 @@ public class PawnMovesCalculator implements MoveRules{
 
     }
 
+    private boolean onBoard(int row, int col) {
+        return row <= 8 && row >= 1 && col <= 8 && col >= 1;
+    }
+
     private void diagonalMoves(Collection<ChessMove> moves, ChessBoard board,
-                               ChessPosition pos, ChessGame.TeamColor teamColor) {
+                               ChessPosition pos, int start, int promo, int direction) {
 
 
     }
 
     private void forwardMoves(Collection<ChessMove> moves, ChessBoard board,
-                              ChessPosition pos, ChessGame.TeamColor teamColor) {
+                              ChessPosition pos, int start, int promo, int direction) {
+        var singleSpacePosition = new ChessPosition(pos.getRow() + direction, pos.getColumn());
+        if (!onBoard(singleSpacePosition.getRow() + direction, singleSpacePosition.getColumn())) {
+            return;
+        }
 
+        if (board.getPiece(singleSpacePosition) == null) {
+            if (singleSpacePosition.getRow() == promo) {
+                promotionMoves(pos, singleSpacePosition, moves);
+            }
+            else {
+                moves.add(new ChessMove(pos, singleSpacePosition, null));
+                if(pos.getRow() == start) {
+                    var twoSpacePosition = new ChessPosition(pos.getRow() + 2 * direction, pos.getColumn());
+                    if(board.getPiece(twoSpacePosition) == null) {
+                        moves.add(new ChessMove(pos, twoSpacePosition, null));
+                    }
+                }
+            }
+        }
     }
 }
