c0d67e36826a98fbb87f10afbcc7df44474ae4d2
Kyle Robinson <ky.robinson72@gmail.com>
2026-02-09 15:43:29 -0700

    Created a UserService class with register, login, and logout functions to be used by the handlers.

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,7 @@
+package service;
+
+public class UserService {
+    public RegisterResult register(RegisterRequest registerRequest) {return null;}
+    public LoginResult login(LoginRequest loginRequest) {return null;}
+    public void logout(LogoutRequest logoutRequest) {}
+}
