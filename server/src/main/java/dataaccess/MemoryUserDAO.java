43f0d0974d589673369e135941434381e6bde019
Kyle Robinson <ky.robinson72@gmail.com>
2026-02-11 15:14:55 -0700

    Implemented functions for MemoryUserDAO using a HashMap

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -2,19 +2,23 @@ package dataaccess;
 
 import model.UserData;
 
+import java.util.HashMap;
+
 public class MemoryUserDAO implements UserDAO{
+    private HashMap<String, UserData> users = new HashMap<>();
+
     @Override
     public void createUser(UserData user) {
-
+        users.put(user.username(), user);
     }
 
     @Override
     public void deleteUsers() {
-
+        users.clear();
     }
 
     @Override
     public void getUser(String username) {
-
+        users.get(username);
     }
 }
