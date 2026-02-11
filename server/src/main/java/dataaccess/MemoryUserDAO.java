abadd56cd992983e310ff652e50c1e95d9df4439
Kyle Robinson <ky.robinson72@gmail.com>
2026-02-11 15:01:09 -0700

    Created a MemoryUserDAO class to handle storage in memory for phase 3

---
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -0,0 +1,20 @@
+package dataaccess;
+
+import model.UserData;
+
+public class MemoryUserDAO implements UserDAO{
+    @Override
+    public void createUser(UserData user) {
+
+    }
+
+    @Override
+    public void deleteUsers() {
+
+    }
+
+    @Override
+    public void getUser(String username) {
+
+    }
+}
