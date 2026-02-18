67615b937d38b06d12cd502318133a8b0cb0c559
Kyle Robinson <ky.robinson72@gmail.com>
2026-02-18 15:54:48 -0700

    Added a createHandlers function to set up each of the paths for the API calls and updated the constructor for Server to create a new UserService and the data access objects for the UserService

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,35 @@
 package server;
 
+import dataaccess.*;
+import handler.RegisterHandler;
 import io.javalin.*;
+import service.UserService;
 
 public class Server {
 
     private final Javalin javalin;
+    private final UserService userService;
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
 
     public Server() {
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        userService = new UserService(userDAO, authDAO);
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        createHandlers(javalin, userService);
 
         // Register your endpoints and exception handlers here.
 
+    }
+
+//    public Server(UserService userService) {
+//        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+//        createHandlers(javalin, userService);
+//    }
+
+    private void createHandlers(Javalin javalin, UserService userService) {
+        javalin.post("/user", new RegisterHandler(userService));
     }
 
     public int run(int desiredPort) {
