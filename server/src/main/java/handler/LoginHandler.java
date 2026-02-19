9081b7a568aa25d299665945a2a9dddc6660dd3b
Kyle Robinson <ky.robinson72@gmail.com>
2026-02-19 15:41:56 -0700

    Created a LoginHandler class to handle login requests, creating LoginRequest instances and passing JSON back to the Server with the result.

---
+++ server/src/main/java/handler/LoginHandler.java
@@ -0,0 +1,30 @@
+package handler;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+import service.LoginRequest;
+import service.LoginResult;
+import service.UserService;
+
+public class LoginHandler implements Handler {
+    private final UserService userService;
+
+    public LoginHandler(UserService userService) {
+        this.userService = userService;
+    }
+    @Override
+    public void handle(@NotNull Context context) throws ResponseException {
+        var loginRequest = new Gson().fromJson(context.body(), LoginRequest.class);
+
+        if (loginRequest.username() == null || loginRequest.password() == null
+                || loginRequest.username().isEmpty() || loginRequest.password().isEmpty()) {
+            throw new ResponseException(ResponseException.Code.BadRequestError, "Error: bad request");
+        }
+        LoginResult result = userService.login(loginRequest);
+        context.status(200);
+        context.json(new Gson().toJson(result));
+    }
+}
