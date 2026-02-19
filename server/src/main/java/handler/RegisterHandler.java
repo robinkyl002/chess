d5f4373a247473a0a450b298a18e5125ccc81596
Kyle Robinson <ky.robinson72@gmail.com>
2026-02-18 23:59:27 -0700

    Updated the handle function in RegisterHandler to throw a Bad Request error if any of the fields are null or an empty string.

--- server/src/main/java/handler/RegisterHandler.java
+++ server/src/main/java/handler/RegisterHandler.java
@@ -1,6 +1,7 @@
 package handler;
 
 import com.google.gson.Gson;
+import exception.ResponseException;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.jetbrains.annotations.NotNull;
@@ -14,9 +15,14 @@ public class RegisterHandler implements Handler {
         this.userService = userService;
     }
 
-    public void handle(@NotNull Context context) {
+    public void handle(@NotNull Context context) throws ResponseException {
         var registerRequest = new Gson().fromJson(context.body(), RegisterRequest.class);
 
+        if (registerRequest.username() == null || registerRequest.password() == null
+                || registerRequest.email() == null || registerRequest.username().isEmpty()
+                || registerRequest.password().isEmpty() || registerRequest.email().isEmpty()) {
+            throw new ResponseException(ResponseException.Code.BadRequestError, "Error: bad request");
+        }
         var result = userService.register(registerRequest);
         context.status(200);
         context.json(new Gson().toJson(result));
