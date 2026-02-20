452e3a32cc763536b84c50469a3e10ef16a4730e
Kyle Robinson <ky.robinson72@gmail.com>
2026-02-19 17:14:55 -0700

    Added a constructor to GameService.java and added throw statements to each of the main functions.

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,8 +1,20 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import exception.ResponseException;
+
 public class GameService {
-    public void createGame() {}
-    public void joinGame() {}
-    public void listGames() {}
-    public void getGame() {}
+    private final AuthDAO authDAO;
+    private final GameDAO gameDAO;
+
+    public GameService(AuthDAO authDAO, GameDAO gameDAO) {
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+    }
+
+    public void createGame() throws ResponseException {}
+    public void joinGame() throws ResponseException {}
+    public void listGames() throws ResponseException {}
+    public void getGame() throws ResponseException {}
 }
