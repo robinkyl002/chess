52a7020ee73917e4b44288ece4281b21c7d04c31
Kyle Robinson <ky.robinson72@gmail.com>
2026-02-19 18:39:45 -0700

    Set up the handle function within the CreateGameHandler class, setting it up to check that a user is authorized and setting up the return values in the Context object.

--- server/src/main/java/handler/CreateGameHandler.java
+++ server/src/main/java/handler/CreateGameHandler.java
@@ -1,11 +1,14 @@
 package handler;
 
+import com.google.gson.Gson;
 import exception.ResponseException;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.jetbrains.annotations.NotNull;
-import service.GameService;
-import service.UserService;
+import service.*;
+
+import static exception.ResponseException.errorMessageFromCode;
+import static exception.ResponseException.Code.*;
 
 public class CreateGameHandler implements Handler {
     private final UserService userService;
@@ -18,6 +21,16 @@ public class CreateGameHandler implements Handler {
 
     @Override
     public void handle(@NotNull Context context) throws ResponseException {
+        String authToken = context.header("Authorization");
+
+        if (!userService.validAuth(authToken)) {
+            throw new ResponseException(UnauthorizedError, errorMessageFromCode(UnauthorizedError));
+        }
+        var createGameRequest = new Gson().fromJson(context.body(), CreateGameRequest.class);
+
+        var result = gameService.createGame(createGameRequest);
+
+        context.status(200).json(new Gson().toJson(result));
 
     }
 }
