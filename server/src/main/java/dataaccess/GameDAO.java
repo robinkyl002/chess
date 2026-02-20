ef4268051769f608b559b1f36df8565e8b00fb78
Kyle Robinson <ky.robinson72@gmail.com>
2026-02-19 17:24:27 -0700

    Added throws DataAccessException to the signature of the functions in GameDAO and MemoryGameDAO. Also added try-catch parts to each of the functions in MemoryGameDAO to throw a DataAccessException when any other error occurs while performing operations.

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -4,9 +4,9 @@ import chess.ChessGame;
 import model.GameData;
 
 public interface GameDAO {
-    int createGame(String gameName);
-    GameData getGame(int gameID);
-    void listGames();
-    void updateGame(int gameID, String username, ChessGame.TeamColor color);
-    void clearGameData();
+    int createGame(String gameName) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    void listGames() throws DataAccessException;
+    void updateGame(int gameID, String username, ChessGame.TeamColor color) throws DataAccessException;
+    void clearGameData() throws DataAccessException;
 }
--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -10,36 +10,57 @@ public class MemoryGameDAO implements GameDAO {
     private int nextId = 1;
 
     @Override
-    public int createGame(String gameName) {
-        var game = new GameData(nextId++, null, null,
-                gameName, new ChessGame());
-        games.put(game.gameID(), game);
-        return game.gameID();
+    public int createGame(String gameName) throws DataAccessException {
+        try {
+            var game = new GameData(nextId++, null, null,
+                    gameName, new ChessGame());
+            games.put(game.gameID(), game);
+            return game.gameID();
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     @Override
-    public GameData getGame(int gameID) {
-        return games.get(gameID);
+    public GameData getGame(int gameID) throws DataAccessException {
+        try {
+            return games.get(gameID);
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     @Override
-    public void listGames() {
+    public void listGames() throws DataAccessException {
+        try {
+
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
 
     }
 
     @Override
-    public void updateGame(int gameID, String username, ChessGame.TeamColor color) {
-        var currGame = getGame(gameID);
-        var newGame = (color == ChessGame.TeamColor.WHITE) ?
-                new GameData(gameID, username, currGame.blackUsername(), currGame.gameName(), currGame.game()) :
-                new GameData(gameID, currGame.whiteUsername(), username, currGame.gameName(), currGame.game());
-        games.put(gameID, newGame);
+    public void updateGame(int gameID, String username, ChessGame.TeamColor color) throws DataAccessException {
+        try {
+            GameData currGame = getGame(gameID);
+            var newGame = (color == ChessGame.TeamColor.WHITE) ?
+                    new GameData(gameID, username, currGame.blackUsername(), currGame.gameName(), currGame.game()) :
+                    new GameData(gameID, currGame.whiteUsername(), username, currGame.gameName(), currGame.game());
+            games.put(gameID, newGame);
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     @Override
-    public void clearGameData() {
-        if(!games.isEmpty()) {
-            games.clear();
+    public void clearGameData() throws DataAccessException {
+        try {
+            if (!games.isEmpty()) {
+                games.clear();
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
         }
     }
 }
