af0d6d1b83d308ea1d4547df513d8c209e8ed773
Kyle Robinson <ky.robinson72@gmail.com>
2026-02-24 21:15:44 -0700

    Created a ListGamesHandler class to interface between Server and the service classes for listGames functionality

---
+++ server/src/main/java/handler/ListGamesHandler.java
@@ -0,0 +1,29 @@
+package handler;
+
+import exception.ResponseException;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+import service.GameService;
+import service.UserService;
+import static exception.ResponseException.Code.*;
+import static exception.ResponseException.errorMessageFromCode;
+
+public class ListGamesHandler implements Handler {
+    private final UserService userService;
+    private final GameService gameService;
+
+    public ListGamesHandler(UserService userService, GameService gameService) {
+        this.userService = userService;
+        this.gameService = gameService;
+    }
+
+    @Override
+    public void handle(@NotNull Context context) throws ResponseException {
+        String authToken = context.header("Authorization");
+
+        if (!userService.validAuth(authToken)) {
+            throw new ResponseException(UnauthorizedError, errorMessageFromCode(UnauthorizedError));
+        }
+    }
+}
