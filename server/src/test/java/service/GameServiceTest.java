08db0c0900c685afe6cb6316b41c8f46a78f7a58
Kyle Robinson <ky.robinson72@gmail.com>
2026-02-25 15:57:40 -0700

    GameServiceTest - Added setup function to run before each test, clearing the games out and setting up a user. Also implemented a positive and negative test for the createGame function within GameService

--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -1,23 +1,47 @@
 package service;
 
+import dataaccess.*;
+import exception.ResponseException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import passoff.model.TestUser;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameServiceTest {
+    private final UserDAO userDAO = new MemoryUserDAO();
+    private final AuthDAO authDAO = new MemoryAuthDAO();
+    private final GameDAO gameDAO = new MemoryGameDAO();
+    private final UserService userService = new UserService(userDAO, authDAO);
+    private final GameService gameService = new GameService(authDAO, gameDAO);
+    private static TestUser existingUser;
+
+    @BeforeEach
+    void setup () throws ResponseException {
+        gameService.clearAllGames();
+
+        existingUser = new TestUser("ExistingUser", "existingPassword", "eu@mail.com");
+    }
 
     @Test
     @Order(1)
     @DisplayName("Create Game Successful")
-    void createGameSuccessful() {
+    void createGameSuccessful() throws ResponseException {
+
+        var createGameResult = gameService.createGame(new CreateGameRequest("newGame"));
+
+        assertNotNull(createGameResult);
+        assertEquals(1, createGameResult.gameID());
     }
 
     @Test
     @Order(2)
     @DisplayName("Create Game - Empty Game Name")
-    void createGameEmptyGameName() {
+    void createGameEmptyGameName() throws ResponseException {
+
+        assertThrows(ResponseException.class, () -> gameService.createGame(new CreateGameRequest("")), "Error: bad request");
     }
 
     @Test
