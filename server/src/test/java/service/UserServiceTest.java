550c37214b13e7ff836f41d19db017c02dd5e2fb
Kyle Robinson <ky.robinson72@gmail.com>
2026-02-24 23:16:02 -0700

    Added a setup function UserServiceTest to run before each test, clearing the storage of UserData and AuthData. Also implemented the first register test

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,9 +1,12 @@
 package service;
 
 import dataaccess.*;
+import exception.ResponseException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import passoff.model.TestUser;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,14 +15,25 @@ class UserServiceTest {
     private final UserDAO userDAO = new MemoryUserDAO();
     private final AuthDAO authDAO = new MemoryAuthDAO();
     private final UserService userService = new UserService(userDAO, authDAO);
-    void setup () {
+    private static TestUser existingUser;
 
+    @BeforeEach
+    void setup () throws ResponseException {
+        userService.clearUserData();
+        userService.clearAuthData();
+
+        existingUser = new TestUser("ExistingUser", "existingUserPassword", "eu@mail.com");
     }
 
     @Test
     @Order(1)
     @DisplayName("Register Successful")
-    void registerSuccessful() {
+    void registerSuccessful() throws ResponseException {
+        var registerResult = userService.register(new RegisterRequest("newUser", "newUserPassword", "nu@mail.com"));
+
+        assertNotNull(registerResult);
+        assertEquals("newUser", registerResult.username());
+        assertNotNull(registerResult.authToken());
     }
 
     @Test
