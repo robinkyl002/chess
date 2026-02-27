ddfadaa079fe575d3684a47399dd685fc7b868ac
Kyle Robinson <ky.robinson72@gmail.com>
2026-02-26 17:50:39 -0700

    ServerMain - Added a try catch to handle any errors that slip through while operating the server. Also updated the output when the Server starts

--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -4,9 +4,13 @@ import server.Server;
 
 public class ServerMain {
     public static void main(String[] args) {
-        int port = (args.length >= 1) ? Integer.parseInt(args[0]) : 8080;
-        Server server = new Server();
-        server.run(port);
-        System.out.println("♕ 240 Chess Server");
+        try {
+            int port = (args.length >= 1) ? Integer.parseInt(args[0]) : 8080;
+            Server server = new Server();
+            server.run(port);
+            System.out.printf("Server started on port %d%n♕ 240 Chess Server", port);
+        } catch (Exception e) {
+            System.out.printf("Unable to start server%s%n", e.getMessage());
+        }
     }
 }
