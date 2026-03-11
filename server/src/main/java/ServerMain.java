24646378cb4dc60b76b423b5efd115121bbf0f77
Kyle Robinson <ky.robinson72@gmail.com>
2026-03-11 15:20:02 -0600

    ServerMain.java - Modified the creation of the DAO objects to base the type of DAO object on whether "sql" was added to the command line arguments

--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -1,12 +1,28 @@
 import chess.*;
 import dataaccess.*;
 import server.Server;
+import service.GameService;
+import service.UserService;
 
 public class ServerMain {
     public static void main(String[] args) {
         try {
             int port = (args.length >= 1) ? Integer.parseInt(args[0]) : 8080;
-            Server server = new Server();
+
+            UserDAO userDAO = new MemoryUserDAO();
+            AuthDAO authDAO = new MemoryAuthDAO();
+            GameDAO gameDAO = new MemoryGameDAO();
+
+            if (args.length >= 2 && args[1].equals("sql")) {
+                userDAO = new SQLUserDAO();
+                authDAO = new SQLAuthDAO();
+                gameDAO = new SQLGameDAO();
+            }
+
+            UserService userService = new UserService(userDAO, authDAO);
+            GameService gameService = new GameService(authDAO, gameDAO);
+            Server server = new Server(userService, gameService);
+
             server.run(port);
             System.out.printf("Server started on port %d%n♕ 240 Chess Server", port);
         } catch (Exception e) {
