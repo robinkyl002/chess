480e41a2951d8bbf46884d36b737ed6321edf7a5
Kyle Robinson <ky.robinson72@gmail.com>
2026-03-11 15:43:50 -0600

    SQLUserDAO - Added a constructor and implemented createUser and deleteUser. Also started implementing getUser based on the process outlined in PetShop to get a Pet

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,35 +1,51 @@
 package dataaccess;
 
+import exception.ResponseException;
 import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class SQLUserDAO implements UserDAO {
+
+    public SQLUserDAO() throws ResponseException {
+        try {
+            DatabaseManager.configureDatabase();
+        } catch (DataAccessException e) {
+            throw new ResponseException(ResponseException.Code.ServerError,
+                    ResponseException.errorMessageFromCode(ResponseException.Code.ServerError));
+        }
+    }
     @Override
     public void createUser(UserData user) throws DataAccessException {
-        try (Connection conn = DatabaseManager.getConnection()) {
-            var statement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
-            try (PreparedStatement ps = conn.prepareStatement(statement)) {
-                
-            }
-        } catch (Exception e) {
-            throw new DataAccessException(e.getMessage());
-        }
+        var statement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+        String hashedPassword = BCrypt.hashpw(user.password(), BCrypt.gensalt());
+        int id = DatabaseManager.executeUpdate(statement, user.username(), hashedPassword, user.email());
     }
 
     @Override
     public void deleteUsers() throws DataAccessException {
-        try {
-
-        } catch (Exception e) {
-            throw new DataAccessException(e.getMessage());
-        }
-
+        var statement = "TRUNCATE user";
+        DatabaseManager.executeUpdate(statement);
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, password, email FROM user WHERE username=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, username);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to retrieve user from database: %s", e.getMessage()), e);
+        }
         return null;
     }
 }
