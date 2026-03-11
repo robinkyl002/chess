776c5655edc00f1f749a2f8d4d63c1173e1cd933
Kyle Robinson <ky.robinson72@gmail.com>
2026-03-11 15:16:23 -0600

    SQLUserDAO - Added try-catch statements to createUser and deleteUsers and I started building out the logic to add a user to the database

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -2,14 +2,29 @@ package dataaccess;
 
 import model.UserData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class SQLUserDAO implements UserDAO {
     @Override
     public void createUser(UserData user) throws DataAccessException {
-
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     @Override
     public void deleteUsers() throws DataAccessException {
+        try {
+
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
 
     }
 
