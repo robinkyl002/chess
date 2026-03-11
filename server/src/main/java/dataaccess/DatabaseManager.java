876d5c84998b248420be72f1e05516891baa2b93
Kyle Robinson <ky.robinson72@gmail.com>
2026-03-11 15:15:14 -0600

    DatabaseManager - Added array of Create statements, as well as a configureDatabase and an executeUpdate method.

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -1,7 +1,12 @@
 package dataaccess;
 
+import exception.ResponseException;
+
 import java.sql.*;
 import java.util.Properties;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class DatabaseManager {
     private static String databaseName;
@@ -14,6 +19,63 @@ public class DatabaseManager {
      */
     static {
         loadPropertiesFromResources();
+    }
+
+    private static final String[] CREATE_STATEMENTS = {
+            """
+            CREATE TABLE IF NOT EXISTS user(
+               `username` varchar(256),
+               `password` varchar(256),
+               `email` varchar(256),
+               PRIMARY KEY (`username`),
+               INDEX(password),
+               INDEX(email)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+            ,
+            """
+            CREATE TABLE IF NOT EXISTS auth(
+                `username` varchar(256),
+                `authToken` varchar(256),
+                PRIMARY KEY (`authToken`),
+                INDEX (username)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : CREATE_STATEMENTS) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()), ex);
+        }
+    }
+
+    static int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (int i = 0; i < params.length; i++) {
+                    Object param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()), e);
+        }
     }
 
     /**
@@ -48,7 +110,7 @@ public class DatabaseManager {
             conn.setCatalog(databaseName);
             return conn;
         } catch (SQLException ex) {
-            throw new DataAccessException("failed to get connection", ex);
+            throw new DataAccessException("Failed to get connection");
         }
     }
 
