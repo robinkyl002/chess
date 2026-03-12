55cff3b1dba471f7cad4170df7b1cdcc2d1b18c5
Kyle Robinson <ky.robinson72@gmail.com>
2026-03-12 00:05:56 -0600

    GameDAOTest.java - Set up the framework for all the positive tests for GameDAO

--- server/src/test/java/dataaccess/AuthDAOTest.java
+++ server/src/test/java/dataaccess/AuthDAOTest.java
@@ -3,7 +3,6 @@ package dataaccess;
 import model.AuthData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import passoff.model.TestUser;
--- server/src/test/java/dataaccess/GameDAOTest.java
+++ server/src/test/java/dataaccess/GameDAOTest.java
@@ -1,33 +1,68 @@
 package dataaccess;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import passoff.model.TestUser;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameDAOTest {
+    private GameDAO getDataAccess(Class<? extends GameDAO> gameDAOClass) {
+        try {
+            GameDAO db;
+            if (gameDAOClass.equals(MemoryGameDAO.class)) {
+                db = new MemoryGameDAO();
+            } else {
+                db = new SQLGameDAO();
+            }
+            db.clearGameData();
+            return db;
+        } catch (Exception e) {
+            throw new RuntimeException("Could not initialize GameDAO for tests");
+        }
+    }
+    private static TestUser existingUser;
 
     @BeforeEach
-    void setUp() {
+    void setup() {
+        existingUser = new TestUser("username", "password", "email");
     }
 
-    @Test
-    void createGame() {
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryGameDAO.class, SQLGameDAO.class})
+    @DisplayName("Create Game Successful")
+    void createGameSuccessful(Class<? extends GameDAO> gameDAOClass) {
+        GameDAO gameDAO = getDataAccess(gameDAOClass);
     }
 
-    @Test
-    void getGame() {
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryGameDAO.class, SQLGameDAO.class})
+    @DisplayName("Retrieve Game Successful")
+    void getGame(Class<? extends GameDAO> gameDAOClass) {
+        GameDAO gameDAO = getDataAccess(gameDAOClass);
     }
 
-    @Test
-    void listGames() {
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryGameDAO.class, SQLGameDAO.class})
+    @DisplayName("List Games Successful")
+    void listGames(Class<? extends GameDAO> gameDAOClass) {
+        GameDAO gameDAO = getDataAccess(gameDAOClass);
     }
 
-    @Test
-    void joinGame() {
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryGameDAO.class, SQLGameDAO.class})
+    @DisplayName("Join Game Successful")
+    void joinGame(Class<? extends GameDAO> gameDAOClass) {
+        GameDAO gameDAO = getDataAccess(gameDAOClass);
     }
 
-    @Test
-    void clearGameData() {
+    @ParameterizedTest
+    @ValueSource(classes = {MemoryGameDAO.class, SQLGameDAO.class})
+    @DisplayName("Clear Games Successful")
+    void clearGameDataSuccessful(Class<? extends GameDAO> gameDAOClass) {
+        GameDAO gameDAO = getDataAccess(gameDAOClass);
+
     }
 }
