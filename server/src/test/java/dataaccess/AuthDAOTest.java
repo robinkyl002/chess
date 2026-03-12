b9918667d63301df3e707d8e23ae6d4659713d98
Kyle Robinson <ky.robinson72@gmail.com>
2026-03-11 23:45:59 -0600

    AuthDAOTest.java, GameDAOTest.java - Created test files for AuthDAO and GameDAO

---
+++ server/src/test/java/dataaccess/AuthDAOTest.java
@@ -0,0 +1,30 @@
+package dataaccess;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthDAOTest {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createAuth() {
+    }
+
+    @Test
+    void getAuth() {
+    }
+
+    @Test
+    void deleteAuth() {
+    }
+
+    @Test
+    void clearAuthData() {
+    }
+}
---
+++ server/src/test/java/dataaccess/GameDAOTest.java
@@ -0,0 +1,33 @@
+package dataaccess;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createGame() {
+    }
+
+    @Test
+    void getGame() {
+    }
+
+    @Test
+    void listGames() {
+    }
+
+    @Test
+    void joinGame() {
+    }
+
+    @Test
+    void clearGameData() {
+    }
+}
