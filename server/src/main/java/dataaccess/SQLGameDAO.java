2a9010ad21ff86c3bab530c1b72f4bf93d88c7c7
Kyle Robinson <ky.robinson72@gmail.com>
2026-03-11 21:11:33 -0600

    SQLGameDAO.java - Added the constructor and implemented the clearGameData function

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,12 +1,23 @@
 package dataaccess;
 
 import chess.ChessGame;
+import exception.ResponseException;
 import model.GameData;
 
 import java.util.Collection;
 import java.util.List;
 
 public class SQLGameDAO implements GameDAO {
+
+    public SQLGameDAO() throws ResponseException{
+        try {
+            DatabaseManager.configureDatabase();
+        } catch (DataAccessException e) {
+            throw new ResponseException(ResponseException.Code.ServerError,
+                    ResponseException.errorMessageFromCode(ResponseException.Code.ServerError));
+        }
+    }
+
     @Override
     public int createGame(String gameName) throws DataAccessException {
         return 0;
@@ -29,6 +40,7 @@ public class SQLGameDAO implements GameDAO {
 
     @Override
     public void clearGameData() throws DataAccessException {
-
+        var statement = "TRUNCATE game";
+        DatabaseManager.executeUpdate(statement);
     }
 }
