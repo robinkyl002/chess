9ec15135ab28f0e4a99da9e2e27be70b01820ef1
Kyle Robinson <ky.robinson72@gmail.com>
2026-03-11 21:33:57 -0600

    SQLGameDAO.java - implemented createGame and getGame, including adding a readGame helper function

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,9 +1,15 @@
 package dataaccess;
 
 import chess.ChessGame;
+import com.google.gson.Gson;
 import exception.ResponseException;
 import model.GameData;
+import model.UserData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
@@ -20,11 +26,26 @@ public class SQLGameDAO implements GameDAO {
 
     @Override
     public int createGame(String gameName) throws DataAccessException {
-        return 0;
+        var statement = "INSERT INTO game (gameName, blackUsername, whiteUsername, chessGame) VALUES (?, ?, ?, ?)";
+        String chessGame = new Gson().toJson(new ChessGame());
+        return  DatabaseManager.executeUpdate(statement, gameName, null, null, chessGame);
     }
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT * FROM game WHERE id=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, gameID);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readGame(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to retrieve game from database: %s", e.getMessage()), e);
+        }
         return null;
     }
 
@@ -42,5 +63,15 @@ public class SQLGameDAO implements GameDAO {
     public void clearGameData() throws DataAccessException {
         var statement = "TRUNCATE game";
         DatabaseManager.executeUpdate(statement);
+    }
+
+    private GameData readGame(ResultSet rs) throws SQLException {
+        int gameID = rs.getInt("id");
+        String whiteUsername = rs.getString("whiteUsername");
+        String blackUsername = rs.getString("blackUsername");
+        String gameName = rs.getString("gameName");
+        String chessGameJson = rs.getString("chessGame");
+        return new GameData(gameID, whiteUsername, blackUsername, gameName,
+                new Gson().fromJson(chessGameJson, ChessGame.class));
     }
 }
