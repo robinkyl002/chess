a965a9d796defd1309a321db767fb3eec914ae63
Kyle Robinson <ky.robinson72@gmail.com>
2026-03-16 14:20:51 -0600

    ResponseException.java - Added fromJson and fromHttpStatusCode methods which will be used by ServerFacade.java

--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -2,7 +2,7 @@ package exception;
 
 import com.google.gson.Gson;
 
-//import java.util.HashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ResponseException extends Exception {
@@ -40,6 +40,27 @@ public class ResponseException extends Exception {
             case BadRequestError -> "Error: bad request";
             case UnauthorizedError -> "Error: unauthorized";
             case AlreadyTakenError -> "Error: already taken";
+        };
+    }
+
+    public static ResponseException fromJson(String json) {
+        var map = new Gson().fromJson(json, HashMap.class);
+        var status = Code.valueOf(map.get("status").toString());
+        String message = map.get("message").toString();
+        return new ResponseException(status, message);
+    }
+
+    public Code code() {
+        return code;
+    }
+
+    public static Code fromHttpStatusCode(int httpStatusCode) {
+        return switch (httpStatusCode) {
+            case 500 -> Code.ServerError;
+            case 400 -> Code.BadRequestError;
+            case 401 -> Code.UnauthorizedError;
+            case 403 -> Code.AlreadyTakenError;
+            default -> throw new IllegalArgumentException("Unknown HTTP status code: " + httpStatusCode);
         };
     }
 }
