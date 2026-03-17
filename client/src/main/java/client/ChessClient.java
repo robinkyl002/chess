c33e0633e4678ce3e2da460e7ba44dbdd45007ca
Kyle Robinson <ky.robinson72@gmail.com>
2026-03-16 18:48:28 -0600

    ChessClient.java - added a help function to identify what should be displayed when the help command is input. Also added an eval method to identify the command called and call the necessary functions.

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,7 +1,10 @@
 package client;
 
+import exception.ResponseException;
 import server.ServerFacade;
+import service.RegisterRequest;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 import static ui.EscapeSequences.*;
@@ -38,6 +41,57 @@ public class ChessClient {
 
     private void printPrompt() {
         System.out.print("\n" + SET_TEXT_COLOR_BLACK + ">>> " + SET_TEXT_COLOR_GREEN);
+    }
+
+    public String eval(String input) {
+        try {
+            String[] tokens = input.toLowerCase().split(" ");
+            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> register(params);
+                case "login" -> null;
+                case "list" -> null;
+                case "logout" -> null;
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String register(String ...params) throws ResponseException {
+        if (params.length >= 3) {
+            var result = server.register(new RegisterRequest(params[0], params[1], params[2]));
+            return "";
+        }
+        throw new ResponseException(ResponseException.Code.BadRequestError, "Expected: <USERNAME> <PASSWORD> <EMAIL>");
+    }
+
+    public String help() {
+        if (state == State.SIGNEDOUT) {
+            return """
+                    - register <USERNAME> <PASSWORD> <EMAIL> - to create an account
+                    - login <USERNAME> <PASSWORD> - to play chess
+                    - quit - playing chess
+                    - help - with possible commands
+                    """;
+        }
+        return """
+                - create <NAME> - to start a new game
+                - join <ID> - to join a game
+                - list - to see all existing games
+                - logout
+                - quit - playing chess
+                - help - with possible commands
+                """;
+    }
+
+    private void assertSignedIn() throws ResponseException {
+        if (state == State.SIGNEDOUT) {
+            throw new ResponseException(ResponseException.Code.UnauthorizedError, "You must sign in");
+        }
     }
 
 }
