7001953c87287cfcc187968a4b94fd0adf71d177
Kyle Robinson <ky.robinson72@gmail.com>
2026-03-17 23:10:42 -0600

    EscapeSequences.java - Added a RESET sequence to set the text color to the default.

--- client/src/main/java/ui/EscapeSequences.java
+++ client/src/main/java/ui/EscapeSequences.java
@@ -7,6 +7,7 @@ public class EscapeSequences {
 
     private static final String UNICODE_ESCAPE = "\u001b";
     private static final String ANSI_ESCAPE = "\033";
+    public static final String RESET = UNICODE_ESCAPE + "[0m";
 
     public static final String ERASE_SCREEN = UNICODE_ESCAPE + "[H" + UNICODE_ESCAPE + "[2J";
     public static final String ERASE_LINE = UNICODE_ESCAPE + "[2K";
