14b25f135f5969e5488836eb87f0bc1002fce488
Kyle Robinson <ky.robinson72@gmail.com>
2026-03-26 17:23:31 -0600

    ServerFacadeTests.java - Set up the init method to create a ServerFacade which connects to the Server

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,17 +2,20 @@ package client;
 
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(String.format("http://localhost:%d", port));
     }
 
     @AfterAll
