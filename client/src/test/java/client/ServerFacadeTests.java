0d66de2515a69c436d24c48050e7d8e510776563
Kyle Robinson <ky.robinson72@gmail.com>
2026-04-01 22:19:36 -0600

    ServerFacadeTests.java - Added Display names for each of the tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -32,48 +32,65 @@ public class ServerFacadeTests {
     }
 
     @Test
+    @DisplayName("Login Successful")
     public void loginSuccess() {
         assertEquals(true, true);
     }
 
     @Test
+    @DisplayName("Login Failed - No Existing User")
     public void loginFail() {}
 
     @Test
+    @DisplayName("Register Successful")
     public void registerSuccess() {
         assertEquals(true, true);
     }
+
     @Test
+    @DisplayName("Register Failed - Already Taken Error")
     public void registerFail() {}
 
     @Test
+    @DisplayName("Logout Successful")
     public void logoutSuccess() {
         assertEquals(true, true);
     }
+
     @Test
+    @DisplayName("Logout Failed - No existing auth token")
     public void logoutFail() {}
 
     @Test
+    @DisplayName("Create Game Successful")
     public void createGameSuccess() {
         assertEquals(true, true);
     }
+
     @Test
+    @DisplayName("Create Game Failed - Empty name")
     public void createGameFail() {}
 
     @Test
+    @DisplayName("Join Game Successful")
     public void joinGameSuccess() {
         assertEquals(true, true);
     }
+
     @Test
+    @DisplayName("Join Game Failed")
     public void joinGameFail() {
 
     }
 
     @Test
+    @DisplayName("List Games Successful")
     public void listGameSuccess() {
         assertEquals(true, true);
     }
+
     @Test
+    @DisplayName("List Games Failed - Empty list")
     public void listGameFail() {}
 
 }
