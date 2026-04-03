3f124fb782f43afa3754ab019f8f4bebf7f99559
Kyle Robinson <ky.robinson72@gmail.com>
2026-04-03 16:27:00 -0600

    ConnectionManager.java, WebsocketHandler.java - Created WebsocketHandler to act as the handler for the Server to handle websocket requests and ConnectionManager to store Sessions

---
+++ server/src/main/java/handler/websocket/WebsocketHandler.java
@@ -0,0 +1,21 @@
+package handler.websocket;
+
+import io.javalin.websocket.*;
+import org.jetbrains.annotations.NotNull;
+
+public class WebsocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+    @Override
+    public void handleClose(@NotNull WsCloseContext wsCloseContext) throws Exception {
+
+    }
+
+    @Override
+    public void handleConnect(@NotNull WsConnectContext wsConnectContext) throws Exception {
+
+    }
+
+    @Override
+    public void handleMessage(@NotNull WsMessageContext wsMessageContext) throws Exception {
+
+    }
+}
