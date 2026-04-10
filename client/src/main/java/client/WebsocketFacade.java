70a47af67a1dc3581fff61291f01772d076a4ba7
Kyle Robinson <ky.robinson72@gmail.com>
2026-04-10 15:30:03 -0600

    WebsocketFacade.java - created WebsocketFacade which can help handle breaking down server messages and pass on any info for the client to use.

---
+++ client/src/main/java/client/WebsocketFacade.java
@@ -0,0 +1,65 @@
+package client;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import jakarta.websocket.*;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WebsocketFacade extends Endpoint {
+    private final Session session;
+    private final NotificationHandler notificationHandler;
+
+    public WebsocketFacade(String url) throws ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.notificationHandler = new NotificationHandler() {
+                @Override
+                public void notify(String notification) {
+
+                }
+            };
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
+
+                    switch (serverMessage.getServerMessageType()) {
+                        case LOAD_GAME -> {
+                            String finalMessage = "";
+                            notificationHandler.notify(finalMessage);
+                        }
+                        case ERROR -> {
+                            String finalMessage = "a";
+                            notificationHandler.notify(finalMessage);
+                        }
+                        case NOTIFICATION ->  {
+                            String finalMessage = "b";
+                            notificationHandler.notify(finalMessage);
+                        }
+                    }
+                }
+            });
+
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(ResponseException.Code.BadRequestError,
+                    ResponseException.errorMessageFromCode(ResponseException.Code.BadRequestError));
+        }
+    }
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+
+    }
+
+    public void joinGameAsPlayer(String message) {}
+    public void joinGameAsObserver() {}
+
+}
