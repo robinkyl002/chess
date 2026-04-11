06b7c37ac3fe9055e7861ff554ea1148057f3311
Kyle Robinson <ky.robinson72@gmail.com>
2026-04-11 17:19:36 -0600

    MakeMoveCommand.java - Created subclass of UserGameCommand to store the extra move information that needs to be included with the command

---
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -0,0 +1,7 @@
+package websocket.commands;
+
+public class MakeMoveCommand extends UserGameCommand{
+    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID) {
+        super(commandType, authToken, gameID);
+    }
+}
