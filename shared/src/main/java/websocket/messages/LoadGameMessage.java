52579b3481bbe15642b19945e85aa2785dda2457
Kyle Robinson <ky.robinson72@gmail.com>
2026-04-11 19:42:38 -0600

    LoadGameMessage.java - Added id field, along with a getter for that field.

--- shared/src/main/java/websocket/messages/LoadGameMessage.java
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -1,9 +1,14 @@
 package websocket.messages;
 
 public class LoadGameMessage extends ServerMessage{
+    private int id;
 
-
-    public LoadGameMessage(ServerMessageType type) {
+    public LoadGameMessage(ServerMessageType type, int id) {
         super(type);
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
     }
 }
