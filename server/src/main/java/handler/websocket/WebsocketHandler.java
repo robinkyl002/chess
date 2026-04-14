ad6fc7e906ed440c066794ff9dcee0a8b1dfd5fc
Kyle Robinson <ky.robinson72@gmail.com>
2026-04-14 17:39:19 -0600

    WebsocketHandler.java - Added a switch-case to handleMessage to call a different method depending on which type of UserGameCommand is entered. also added and began implementing a connect method to be used when joining either as a player or an observer

--- server/src/main/java/handler/websocket/WebsocketHandler.java
+++ server/src/main/java/handler/websocket/WebsocketHandler.java
@@ -1,9 +1,31 @@
 package handler.websocket;
 
+import com.google.gson.Gson;
+import exception.ResponseException;
 import io.javalin.websocket.*;
+import model.SessionData;
+import org.eclipse.jetty.websocket.api.Session;
 import org.jetbrains.annotations.NotNull;
+import service.*;
+import websocket.commands.ConnectCommand;
+import websocket.commands.UserGameCommand;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.Notification;
+import websocket.messages.ServerMessage;
+
+import static exception.ResponseException.Code.*;
+import static exception.ResponseException.errorMessageFromCode;
 
 public class WebsocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+    private ConnectionManager connectionManager = new ConnectionManager();
+    private final GameService gameService;
+    private final UserService userService;
+
+    public WebsocketHandler(GameService gameService, UserService userService) {
+        this.gameService = gameService;
+        this.userService = userService;
+    }
+
     @Override
     public void handleClose(@NotNull WsCloseContext ctx) {
         System.out.println("Websocket connection closed");
@@ -17,10 +39,44 @@ public class WebsocketHandler implements WsConnectHandler, WsMessageHandler, WsC
 
     @Override
     public void handleMessage(@NotNull WsMessageContext ctx) throws Exception {
+        var userGameCommand = new Gson().fromJson(ctx.message(), UserGameCommand.class);
 
+        switch (userGameCommand.getCommandType()) {
+            case CONNECT -> {
+                var connectCommand = new Gson().fromJson(ctx.message(), ConnectCommand.class);
+                connect(connectCommand, ctx.session);
+            }
+            case MAKE_MOVE -> {
+                makeMove();
+            }
+            case LEAVE ->  {
+                leave();
+            }
+            case RESIGN -> {
+                resign();
+            }
+        }
     }
 
-    private void connect() {
+    private void connect(ConnectCommand connectCommand, Session session) throws ResponseException {
+        if (!userService.validAuth(connectCommand.getAuthToken())) {
+            throw new ResponseException(UnauthorizedError, errorMessageFromCode(UnauthorizedError));
+        }
+
+        var currGame = gameService.retrieveGame(connectCommand.getGameID());
+
+        connectionManager.add(session, new SessionData(connectCommand.getAuthToken(), connectCommand.getGameID(),
+                connectCommand.isObserver()));
+        var message = (connectCommand.isObserver()) ?
+                String.format("%s is now observing the game", connectCommand.getUsername()) :
+                String.format("%s is now playing the game", connectCommand.getUsername());
+
+        var notification = new Notification(ServerMessage.ServerMessageType.NOTIFICATION, message);
+
+        connectionManager.broadcast(session, notification, connectCommand.getGameID());
+
+        LoadGameMessage loadGameMessage = new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, currGame);
+        connectionManager.personalMessage(session, loadGameMessage);
 
     }
 
