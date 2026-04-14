cc30bf6a93f1ec332694080f07b1c260db4fef79
Kyle Robinson <ky.robinson72@gmail.com>
2026-04-14 17:37:15 -0600

    ConnectionManager.java - Added methods broadcast and personalMessage to help make sure that all other players get a notification when one player does something and the person performing an action can receive a message that only they need

--- server/src/main/java/handler/websocket/ConnectionManager.java
+++ server/src/main/java/handler/websocket/ConnectionManager.java
@@ -1,7 +1,12 @@
 package handler.websocket;
 
+import com.google.gson.Gson;
+import exception.ResponseException;
 import model.SessionData;
 import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+import static exception.ResponseException.Code.ServerError;
+import static exception.ResponseException.errorMessageFromCode;
 
 import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
@@ -17,14 +22,36 @@ public class ConnectionManager {
         connections.remove(session);
     }
 
-//    public void broadcast(Session excludeSession, Notification notification) throws IOException {
-//        String msg = notification.toString();
-//        for (Session c : connections.values()) {
-//            if (c.isOpen()) {
-//                if (!c.equals(excludeSession)) {
-//                    c.getRemote().sendString(msg);
-//                }
-//            }
-//        }
-//    }
+    public void broadcast(Session excludeSession, ServerMessage message, int gameID) throws ResponseException {
+
+        try {
+            String msg = new Gson().toJson(message);
+            for (var c : connections.entrySet()) {
+                var session = c.getKey();
+                var data = c.getValue();
+
+                if (data.gameID() == gameID) {
+                    if (session.isOpen()) {
+                        if (!session.equals(excludeSession)) {
+                            session.getRemote().sendString(msg);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(ServerError, errorMessageFromCode(ServerError));
+        }
+    }
+
+    public void personalMessage(Session session, ServerMessage message) throws ResponseException {
+        try {
+            String msg = new Gson().toJson(message);
+
+            if (session.isOpen()) {
+                session.getRemote().sendString(msg);
+            }
+        } catch (Exception e) {
+            throw new ResponseException(ServerError, errorMessageFromCode(ServerError));
+        }
+    }
 }
