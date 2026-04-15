56b50ea6f8aafae4d17b0c979b608b43aa4de08e
Kyle Robinson <ky.robinson72@gmail.com>
2026-04-15 17:41:42 -0600

    MakeMoveCommand.java - Added a playerColor field to help make sure a person is allowed to make a move.

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -1,15 +1,18 @@
 package websocket.commands;
 
+import chess.ChessGame;
 import chess.ChessMove;
 
 public class MakeMoveCommand extends UserGameCommand {
     private final ChessMove move;
     private final String username;
+    private final ChessGame.TeamColor playerColor;
 
-    public MakeMoveCommand(String authToken, Integer gameID, ChessMove move, String username) {
+    public MakeMoveCommand(String authToken, Integer gameID, ChessMove move, String username,  ChessGame.TeamColor playerColor) {
         super(CommandType.MAKE_MOVE, authToken, gameID);
         this.move = move;
         this.username = username;
+        this.playerColor = playerColor;
     }
 
     public ChessMove getMove() {
@@ -17,5 +20,8 @@ public class MakeMoveCommand extends UserGameCommand {
     }
     public String getUsername() {
         return username;
+    }
+    public ChessGame.TeamColor getPlayerColor() {
+        return playerColor;
     }
 }
